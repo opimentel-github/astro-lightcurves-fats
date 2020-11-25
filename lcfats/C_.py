60999c2e65605b8117bccc6c2d6f306a0eb97837
oscar pimentel <oscarlo.pimentel@gmail.com>
2020-11-24 21:56:25 -0300

    bspline

--- lcfats/C_.py
+++ lcfats/C_.py
@@ -3,7 +3,7 @@ import numpy as np
 
 ###################################################################################################################################################
 
-N_JOBS = 4 # The number of jobs to use for the computation. If -1 all CPUs are used. If 1 is given, no parallel computing code is used at all, which is useful for debugging. For n_jobs below -1, (n_cpus + 1 + n_jobs) are used. Thus for n_jobs = -2, all CPUs but one are used.
+N_JOBS = 6 # The number of jobs to use for the computation. If -1 all CPUs are used. If 1 is given, no parallel computing code is used at all, which is useful for debugging. For n_jobs below -1, (n_cpus + 1 + n_jobs) are used. Thus for n_jobs = -2, all CPUs but one are used.
 CHUNK_SIZE = N_JOBS*1
 NAN_VALUE = -999
 
