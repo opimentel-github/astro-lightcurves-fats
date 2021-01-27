a48fb1ddcfda427680345191a8130f635d7a907b
opimentel-tesla <oscarlo.pimentel@gmail.com>
2021-01-27 13:23:19 -0300

    redo mcmc

--- experiments/generate_fats_features.run.py
+++ experiments/generate_fats_features.run.py
@@ -10,7 +10,7 @@ if __name__== '__main__':
 	from flamingchoripan.prints import print_big_bar
 
 	parser = argparse.ArgumentParser('usage description')
-	parser.add_argument('-method',  type=str, default='all', help='method')
+	parser.add_argument('-method',  type=str, default='.', help='method')
 	main_args = parser.parse_args()
 	print_big_bar()
 
@@ -32,9 +32,8 @@ if __name__== '__main__':
 		return load_pickle(filename)
 
 	methods = main_args.method
-	if methods=='all':
-		methods = ['linear-nostw', 'bspline-nostw', 'curvefit-nostw', 'mcmc-nostw', 'curvefit', 'mcmc']
-		methods = ['curvefit-nostw', 'mcmc-nostw', 'curvefit', 'mcmc']
+	if methods=='.':
+		methods = ['linear-fstw', 'bspline-fstw', 'spm-mle-fstw', 'spm-mcmc-fstw', 'spm-mle-estw', 'spm-mcmc-estw']
 
 	if isinstance(methods, str):
 		methods = [methods]
