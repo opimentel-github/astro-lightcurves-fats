c7f0fa8253c51f58467e7d1c877f10cb91417fd6
opimentel-deepspace <oscarlo.pimentel@gmail.com>
2021-05-15 13:01:16 -0400

    long run after crash

--- experiments/master_run.sh
+++ experiments/master_run.sh
@@ -2,10 +2,10 @@
 SECONDS=0
 #python generate_fats_features.py
 #python export_2dprojections.py -mode all
+#python export_2dprojections.py -mode spm
 #python export_2dprojections.py -mode sne
 #python train_rf_models.py -mode all
 python train_rf_models.py -mode spm
 python train_rf_models.py -mode sne
 
-mins=$((SECONDS/60))
-echo echo "Time Elapsed : ${mins} minutes"
+echo echo "Time Elapsed : ${$((SECONDS/60))} minutes"
